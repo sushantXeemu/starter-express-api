bdb1dee494dfc86bec94f071d2cdc98037685342
Sushant Xeemu <sushant.xeemu@gmail.com>
2023-02-28 09:46:53 +0000

    new fix

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
-const app = express()
+const app = express();
+const os = require('os');
+const systemCpuCores = os.cpus();
+require('dotenv').config()
+
+
+
+
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo Yo!!!!')
-})
+    console.log("Just got a request!");
+    console.log(process.env.GOOGLE_KEY);
+    res.jsonp((systemCpuCores) );
+});
+
+
 app.listen(process.env.PORT || 3000)
